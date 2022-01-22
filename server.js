a889fe88d1d8b50f71e786d2a4c530308ae5bbf2
Dejanimitrijevic <ddimitrijevicdev@gmail.com>
2022-01-22 08:08:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const auth = require('./routes/auth');
 const app = express();
 app.use(cors({ credentials: true, origin: true }));
 app.use(function (req, res, next) {
-  // res.header('Access-Control-Allow-Origin', 'https://mebook-app.netlify.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Credentials', true);
   res.header(
