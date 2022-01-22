9eae233a28e80606024ca816ac93e94091d58b8b
Dejanimitrijevic <ddimitrijevicdev@gmail.com>
2022-01-22 06:31:58 +0200

    update

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const express = require('express');
-// const morgan = require('morgan');
-// const cors = require('cors');
-// const cookieParser = require('cookie-parser');
-// const auth = require('./routes/auth');
+const morgan = require('morgan');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const auth = require('./routes/auth');
 
 // INITIALIZE API EXPRESS SERVER
 const app = express();
-// app.use(cors({ credentials: true, origin: true }));
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-
-//   next();
-// });
+app.use(cors({ credentials: true, origin: true }));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+  next();
+});
 
 // API SERVER MIDDLEWARS
-// app.use(express.json());
-// app.use(morgan('dev'));
-// app.use(cookieParser());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cookieParser());
 
 // API AUTHENTICATION ROUTE
-// app.use('/api/auth', auth);
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'MEBOOK API SERVER' });
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'MEBOOK' });
 });
+app.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 4040;
 app.listen(PORT, () => {
